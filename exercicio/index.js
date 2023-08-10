ab4d2c9acc020e534ee2858cefd08ae4b3260a54
Jeancarlos Britto de Bratti <jeancarlosbrattidebritto@gmail.com>
2023-08-10 17:25:32 -0300

    exercio nao acabado

---
+++ exercicio/index.js
@@ -0,0 +1,52 @@
+var matriz = [[p1,p2,p3],[p4,p5,p6],[p7,p8,p9]
+                ];
+var p1,p2,p3,p4,p5,p6,p7,p8,p9;
+var teclado = 0;
+var chave = 0;
+var dalay = 0;
+var macumba;
+chave = 0; 
+teclado = 0;
+if (chave == 0){
+   console.log("Escolha um numero diferente que zero para criar uma matriz aleatoria");
+   console.log("Escolha um numero: ");
+   chave = 1;
+}
+teclado = 24;
+delay = 1000;
+if( teclado > 0 | teclado < 0){
+  if(delay >=1000){
+   resuldado = teclado + (delay / teclado);
+   dad0 = 5000 / teclado;
+  } else if(delay < 0){
+   resuldado = teclado + (delay * 2);
+   dado = teclado * 3;
+
+  }else if(delay > 0 & delay < 1000){
+    resuldado = teclado + (delay / 2);
+    dado = 6500 / teclado;
+  } else if (delay == 0){
+    resuldado = (delay * teclado) + teclado;
+    dado = 100 / (teclado + 1);
+  }
+  if(teclado > 0 & teclado < 99){
+    
+    for(var l = 0; l > 10; l++){
+     macunba = p + l;
+
+     matriz[macunba];
+    }
+
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
